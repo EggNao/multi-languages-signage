17cf7dd4800b38a40f9d6a9562d07e9eff1f48a0
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-10-30 10:24:50 +0900

    firebaseと接続

---
+++ src/plugins/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from '@firebase/app'
+import { connectAuthEmulator, getAuth } from '@firebase/auth'
+import { connectFirestoreEmulator, getFirestore, initializeFirestore } from '@firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID ?? '',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? '',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_BUCKET ?? '',
+}
+
+const app = initializeApp(firebaseConfig)
+initializeFirestore(app, {
+  ignoreUndefinedProperties: true,
+})
+
+export const auth = getAuth()
+export const firestore = getFirestore()
