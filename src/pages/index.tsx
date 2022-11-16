3e01078f5bf802237f13797c58e310a512f659c7
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-11-16 09:15:11 +0900

    microcmsの画像表示

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,31 @@
-import type { GetStaticProps, NextPage } from 'next'
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { client } from '../plugins/microcms'
 import styles from '../styles/Home.module.css'
+import { ContentsType } from '../types/microcms'
 
-const Home: NextPage = ( data ) => {
-  console.log(data)
-  return <div>sample</div>
+type HomeProps = {
+  data: ContentsType
 }
 
-export const getStaticProps = async () => {
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ data }) => {
+  console.log(data[0].content.url)
+  const imgSrc = data[0].content.url
+  return (
+    <div>
+        {/* {data.map((doc: ContentsType) => {
+          console.log(doc)
+          return <Image src={doc.content.url} width={720} height={405} alt={''} />
+        })} */}
+        <Image src={imgSrc} width={720} height={405} alt={''} />
+        <Image src={imgSrc} width={720} height={405} alt={''} />
+    </div>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
   const data = await client.get({ endpoint: 'contents' })
-  console.log(data)
 
   return {
     props: {
