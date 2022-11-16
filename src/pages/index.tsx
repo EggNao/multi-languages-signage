c04237d06d0a00f358b0f193b6519354ec0f45b9
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-11-16 12:58:16 +0900

    'swiper/css'のerror修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,14 @@
+import 'swiper/swiper.min.css'
+
 import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import Image from 'next/image'
+import { Autoplay } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import { client } from '../plugins/microcms'
 import { ContentsType } from '../types/microcms'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import { Autoplay } from 'swiper'
-import 'swiper/css'
-
-type HomeProps = {
-  data: ContentsType
-}
 
 const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ data }) => {
+  console.log(data)
   console.log(data[0].content.url)
   return (
     <Swiper
@@ -25,7 +24,13 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ data }
       {data.map((doc: ContentsType, index: number) => {
         return (
           <SwiperSlide key={index}>
-            <img className={'h-screen w-screen'} src={doc.content.url} width={720} height={405} alt={''} />
+            <Image
+              className={'h-screen w-screen'}
+              src={doc.content.url}
+              width={720}
+              height={405}
+              alt={''}
+            />
           </SwiperSlide>
         )
       })}
