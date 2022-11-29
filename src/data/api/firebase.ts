60a17ed26ab0f361ff5dfb9fee3e4f37bfada285
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-11-29 12:40:03 +0900

    表示する言語を算出するAPIを作成

--- src/data/api/firebase.ts
+++ src/data/api/firebase.ts
@@ -1,3 +1,4 @@
+import { GetTLDType } from '@/types/firestore'
 import { query, collection, getDocs } from 'firebase/firestore'
 import { firestore } from '../../plugins/firebase'
 
@@ -8,4 +9,33 @@ export const getTLDNumber = async () => {
   querySnapshot.forEach((doc) => {
     console.log(doc.id, ' => ', doc.data())
   })
+}
+
+export const filterCctld = (tldData: GetTLDType) => {
+  let japaneseTld = 0
+  let englishTld = 0
+  Object.keys(tldData).forEach((tld) => {
+    if (tld == 'jp') {
+      japaneseTld = tldData[tld]
+    } else if (
+      tld == 'kr' ||
+      tld == 'au' ||
+      tld == 'cn' ||
+      tld == 'id' ||
+      tld == 'in' ||
+      tld == 'th' ||
+      tld == 'tw' ||
+      tld == 'uk'
+    )
+      englishTld += tldData[tld]
+  })
+  console.log('englishTld', englishTld)
+  console.log('japaneseTld', japaneseTld)
+  if (8 * englishTld <= japaneseTld){
+    return 'japanese'
+  }
+  else{
+    return 'english'
+  }
+
 }
