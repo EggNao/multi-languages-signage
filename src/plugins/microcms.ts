9cf3e16d4884806067a23b35afa9feb40c601b6a
Naoki Eguchi <eguchi.naoki900@mail.kyutech.jp>
2022-11-29 12:39:24 +0900

    microcmsからfiltersでデータを取得する関数作成

--- src/plugins/microcms.ts
+++ src/plugins/microcms.ts
@@ -1,6 +1,16 @@
 import { createClient } from 'microcms-js-sdk'
+import { ContentsType } from '@/types/microcms'
 
 export const client = createClient({
   apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY ?? '',
   serviceDomain: process.env.NEXT_PUBLIC_MICROCMS_SERVICE_DOMAIN ?? '',
 })
+
+export const getMicroCMSContents = async(language: 'japanese' | 'english'): Promise<ContentsType[]> => {
+  const data = await client.get({
+    endpoint: 'contents',
+    queries: { filters: `language[contains]${language}` }
+  })
+  return data.contents
+}
+
